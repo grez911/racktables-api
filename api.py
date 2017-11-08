fcc1d70ae04f4360763a3676532c635ce11eb050
grez911 <grez911@gmail.com>
2017-11-08 15:01:29 +0300

    Added description to config.py

--- api.py
+++ api.py
@@ -6,4 +6,20 @@ import ipaddr
 
 import config
 
-print('test')
+def db_connect():
+    try:
+        return MySQLdb.connect(
+            host=config.host,
+            port=config.port,
+            passwd=config.passwd,
+            db=config.db,
+            user=config.user)
+    except:
+        print("Can't connect to database. Check that MySQL is accessible via "
+            + "3306 port with requisites from config.py file")
+        sys.exit(1)
+
+db = db_connect()
+db.db_query_all("SELECT name FROM Object")
+r = db.store_result()
+print(r)
--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+'''
+Requisites for database connection
+'''
 
 host = '127.0.0.1'
 port = 3306
-passwd = 'xxxx'
-db = 'xxxx'
-user = 'xxxx'
+passwd = 'xxxxxx'
+db = 'xxxxxx'
+user = 'xxxxxx'
