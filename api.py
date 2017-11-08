7102779ad093e957e345d4fe749564e314d5c7fb
grez911 <grez911@gmail.com>
2017-11-08 14:09:39 +0300

    Added config file

--- api.py
+++ api.py
@@ -1,5 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import MySQLdb
 import sys
+import re
+import ipaddr
+
+import config
 
 print('test')
---
+++ config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+host = '127.0.0.1'
+port = 3306
+passwd = 'xxx'
+db = 'xxx'
+user = 'xxx'
