1c3df29e61d2b431ae7c76f9a8324ae7a8b2736f
grez911 <grez911@gmail.com>
2017-11-23 22:25:47 +0300

    Added get_server_id

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ API for racktables database.
 '''
 
 def get_servers():
+    '''Get all servers from racktables objects page.'''
     return sql.get_servers()
+
+def get_values_by_attr(attr, server):
+    '''Return all values for a given attribute for server'''
+    #return sql.get_values_by_attr()
+    return sql.get_server_id(server)
 
 def print_array(array):
     '''Print array in column'''
@@ -29,6 +35,8 @@ def main():
         parser.error("get_hdds requires --server or -s")
     if args.command == 'get_servers':
         print_array(get_servers())
+    if args.command == 'get_hdds':
+        print_array(get_values_by_attr('HDD', args.server))
 
 if __name__ == "__main__":
     main()
--- sql.py
+++ sql.py
@@ -45,11 +45,21 @@ def get_servers():
     '''Get all servers from racktables objects page.'''
     return db_query_all("SELECT name FROM Object WHERE objtype_id=4")
 
+def get_server_id(name):
+    '''Translate server name to its id'''
+    sql = "SELECT id FROM Object WHERE name = '{}'".format(name)
+    result = db_query_all(sql)
+    if result != None:
+        server_id = result[0]
+    else:
+        server_id = None
+    return server_id
+
 def get_hdds(server_name):
     '''Return all HDD's of the server'''
 
-def get_fqdn(server_name):
-    '''Return FQDN of the server'''
-    server_id = db_query_all("SELECT id FROM Object WHERE name='{}'"
-        .format(server_name))[0]
-    return server_id
+# def get_fqdn(server_name):
+    # '''Return FQDN of the server'''
+    # server_id = db_query_all("SELECT id FROM Object WHERE name='{}'"
+        # .format(server_name))[0]
+    # return server_id
