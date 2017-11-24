cd7ab82d58ae0a630cffb6917e2a9700e6cbc535
grez911 <grez911@gmail.com>
2017-11-24 15:57:50 +0300

    get_hdds now working

--- main.py
+++ main.py
@@ -9,15 +9,6 @@ import sql
 API for racktables database.
 '''
 
-def get_servers():
-    '''Get all servers from racktables objects page.'''
-    return sql.get_servers()
-
-def get_values_by_attr(attr, server):
-    '''Return all values for a given attribute for server'''
-    #return sql.get_values_by_attr()
-    return sql.get_server_id(server)
-
 def print_array(array):
     '''Print array in column'''
     try:
@@ -34,9 +25,9 @@ def main():
     if args.command == 'get_hdds' and args.server is None:
         parser.error("get_hdds requires --server or -s")
     if args.command == 'get_servers':
-        print_array(get_servers())
+        print_array(sql.get_servers())
     if args.command == 'get_hdds':
-        print_array(get_values_by_attr('HDD', args.server))
+        print_array(sql.get_attr_values('HDD', args.server))
 
 if __name__ == "__main__":
     main()
--- sql.py
+++ sql.py
@@ -22,7 +22,7 @@ def db_connect():
         return (dbconn, dbcursor)
     except:
         print("Can't connect to database. Check that MySQL is accessible via "
-            + "3306 port with requisites from config.py file.")
+              "3306 port with requisites from config.py file.")
         sys.exit(1)
 
 def db_close(dbconn, dbcursor):
@@ -49,14 +49,31 @@ def get_server_id(name):
     '''Translate server name to its id'''
     sql = "SELECT id FROM Object WHERE name = '{}'".format(name)
     result = db_query_all(sql)
-    if result != None:
-        server_id = result[0]
-    else:
-        server_id = None
-    return server_id
+    return result
 
-def get_hdds(server_name):
-    '''Return all HDD's of the server'''
+def get_attr_ids(name):
+    '''Return ids of given attribute name'''
+    sql = "SELECT id FROM Attribute WHERE name LIKE '%{}%'".format(name)
+    result = db_query_all(sql)
+    return result
+
+def get_attr_values(attr, server):
+    '''Return all values for a given attribute for server'''
+    result = []
+    for attr_id in get_attr_ids(attr):
+        try:
+            server_id = get_server_id(server)[0]
+            sql = ("SELECT uint_value FROM AttributeValue WHERE "
+                   "object_id = {} AND attr_id = {}".format(server_id, attr_id))
+            dict_key = db_query_all(sql)[0]
+            sql = ("SELECT dict_value FROM Dictionary WHERE dict_key = '{}'"
+                       .format(dict_key))
+            query = db_query_all(sql)
+            if query != ['EMPTY']:
+                result.extend(db_query_all(sql))
+        except:
+            pass
+    return result
 
 # def get_fqdn(server_name):
     # '''Return FQDN of the server'''
