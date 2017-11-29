51803519014e96273dcca09e15763adb5225cec0
grez911 <grez911@gmail.com>
2017-11-29 11:21:37 +0300

    Separated command to action and argument

--- main.py
+++ main.py
@@ -18,16 +18,48 @@ def print_array(array):
         print(array)
 
 def main():
-    parser = argparse.ArgumentParser(description="A CLI API for racktables database.")
-    parser.add_argument('command', choices=['get_servers', 'get_hdds'], help="Command.")
+    parser = argparse.ArgumentParser(
+        description="A CLI API for racktables database.")
+    parser.add_argument('command', help="Command.",
+        choices=['get', 'add', 'del', 'set'], type=str.lower)
+    parser.add_argument('arg', help="Object for the command.",
+        choices=[
+            'HDD',
+            'CPU',
+            'FQDN',
+            'OS',
+            'TAG',
+            'PORT',
+            'STATUS',
+            'SERVERS'
+        ], type=str.upper
+    )
     parser.add_argument('-s', '--server', help="Server for the command.")
+    parser.add_argument('-v', '--value', help="Value for the command.")
     args = parser.parse_args()
-    if args.command == 'get_hdds' and args.server is None:
-        parser.error("get_hdds requires --server or -s")
-    if args.command == 'get_servers':
-        print_array(sql.get_servers())
-    if args.command == 'get_hdds':
-        print_array(sql.get_attr_values('HDD', args.server))
+
+    if (args.arg in
+        ['HDD', 'CPU', 'FQDN', 'OS', 'TAG', 'PORT', 'STATUS']
+        and args.server is None):
+        parser.error("{} requires --server or -s option."
+            .format(args.arg))
+
+    if (args.command in ['add', 'del', 'set']
+        and args.value is None):
+        parser.error("{} requires --value or -v option."
+            .format(args.arg))
+    
+    if args.command == 'get':
+        if args.arg.lower() == 'servers':
+            print_array(sql.get_servers())
+        if args.arg in ['HDD', 'CPU']:
+            print_array(sql.get_attr_values(args.arg, args.server))
+        # if args.arg == 'FQDN':
+            # print_array(sql.get_fqdn(args.server))
+
+    if args.command == 'add':
+        if args.arg in ['HDD', 'CPU']:
+            sql.add_attr_value(args.arg, args.server, args.value)
 
 if __name__ == "__main__":
     main()
--- sql.py
+++ sql.py
@@ -30,6 +30,13 @@ def db_close(dbconn, dbcursor):
     dbcursor.close()
     dbconn.close()
 
+def db_commit(query):
+    '''SQL INSERT or UPDATE function.'''
+    (dbconn, dbcursor) = db_connect()
+    dbcursor.execute(query)
+    dbconn.commit()
+    db_close(dbconn, dbcursor)
+
 def db_query_all(query):
     '''SQL query function, return all rows.'''
     (dbconn, dbcursor) = db_connect()
@@ -46,19 +53,32 @@ def get_servers():
     return db_query_all("SELECT name FROM Object WHERE objtype_id=4")
 
 def get_server_id(name):
-    '''Translate server name to its id'''
+    '''Translate server name to its id.'''
     sql = "SELECT id FROM Object WHERE name = '{}'".format(name)
     result = db_query_all(sql)
     return result
 
 def get_attr_ids(name):
-    '''Return ids of given attribute name'''
+    '''Return ids of given attribute name.'''
     sql = "SELECT id FROM Attribute WHERE name LIKE '%{}%'".format(name)
     result = db_query_all(sql)
     return result
 
+def get_empty_attr_id(name, server):
+    '''Return id of the empty attribute slot for a given server.'''
+    for attr_id in get_attr_ids(name):
+        try:
+            server_id = get_server_id(server)[0]
+            sql = ("SELECT uint_value FROM AttributeValue WHERE "
+                   "object_id = {} AND attr_id = {}".format(server_id, attr_id))
+            query = db_query_all(sql)
+            if query == []:
+                return attr_id
+        except:
+            pass
+
 def get_attr_values(attr, server):
-    '''Return all values for a given attribute for server'''
+    '''Return all values for a given attribute for server.'''
     result = []
     for attr_id in get_attr_ids(attr):
         try:
@@ -74,6 +94,23 @@ def get_attr_values(attr, server):
         except:
             pass
     return result
+
+def add_attr_value(attr, server, value):
+    '''Add a hardware into server.'''
+    try:
+        attr_id = get_empty_attr_id(attr, server)
+        sql = ("SELECT dict_key FROM Dictionary WHERE dict_value = '{}'"
+               .format(value))
+        uint_value = db_query_all(sql)[0]
+        server_id = get_server_id(server)[0]
+        sql = ("INSERT INTO AttributeValue "
+               "(object_id, object_tid, attr_id, uint_value) "
+               "VALUES ({}, 4, {}, {})".format(server_id, attr_id, uint_value))
+        # print(sql)
+        print(db_commit(sql))
+        print('OK')
+    except Exception as e:
+        print('Error: {}'.format(e))
 
 # def get_fqdn(server_name):
     # '''Return FQDN of the server'''
